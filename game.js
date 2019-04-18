64c86b40fdd5ffcf4a8862bf143ebeab5ccf2dc3
SuryaChaitanya Peddineni <43539590+psuryachaitanya@users.noreply.github.com>
2019-04-18 17:17:44 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,18 @@
+var secreatNumber = 7;
+
+var gussNumber= Number(prompt("Guess the Number in between 0 to 9"));
+
+if(Number(gussNumber) === secreatNumber){
+
+    alert("Your Right..!!");
+}
+else if (Number(gussNumber) > secreatNumber ){
+
+   alert("Too High,Guss again");
+   
+}
+
+else{
+
+    alert("Too Low,Guss agin");
+}
